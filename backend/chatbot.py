ce6045944d290bec55d9186152355630ec1a069f
KevinBohorquez <kevinbohorquez0211@gmail.com>
2024-12-02 23:48:32 -0500

    auxiliooo... intento 8

--- backend/chatbot.py
+++ backend/chatbot.py
@@ -46,7 +46,7 @@ def get_response(tag, intents_json):
             return random.choice(i['responses'])
     return "Lo siento, no entiendo esa pregunta."
 
-@app.route('https://matedisproyectorhamses.up.railway.app/chat', methods=['POST'])
+@app.route('/About', methods=['POST'])
 def chat():
     data = request.get_json()
     message = data.get("message")
