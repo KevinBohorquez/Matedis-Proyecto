b18948415b9c46809b5854d09e903632defb5f41
KevinBohorquez <kevinbohorquez0211@gmail.com>
2024-12-06 18:26:00 -0500

    xd

--- src/Chatbot.js
+++ src/Chatbot.js
@@ -6,6 +6,7 @@ function Chatbot() {
 
   const sendMessage = async () => {
     try {
+      console.log('Enviando solicitud...');
       const response = await fetch('https://pruebamatedis.up.railway.app/random-number', {
         method: 'GET',
         headers: {
@@ -18,11 +19,30 @@ function Chatbot() {
       }
   
       const data = await response.json();
+      console.log('Respuesta de la API:', data); // Revisa la respuesta de la API
       setRandomNumber(data.random_number); // Guarda el número aleatorio en el estado
     } catch (error) {
       console.error('Error:', error);
     }
-  };  
+  };
+
+  return (
+    <div>
+      <h1>Generador de Número Aleatorio</h1>
+      <input
+        type="text"
+        placeholder="Escribe un mensaje"
+        value={message}
+        onChange={(e) => setMessage(e.target.value)}
+      />
+      <button onClick={sendMessage}>Enviar mensaje</button>
+      
+      {/* Muestra el número aleatorio solo cuando esté disponible */}
+      {randomNumber !== null && (
+        <p>El número aleatorio generado es: {randomNumber}</p>
+      )}
+    </div>
+  );
 }
 
 export default Chatbot;
