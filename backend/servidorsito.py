0d002f34f92274554afa893b982384935c6590de
KevinBohorquez <kevinbohorquez0211@gmail.com>
2024-12-08 23:51:00 -0500

    ayuda

--- backend/servidorsito.py
+++ backend/servidorsito.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
@@ -7,4 +8,6 @@ def hola_mundo():
     return 'Hola Mundo desde /prueba'
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Railway establece el puerto a través de la variable de entorno PORT
+    port = int(os.environ.get("PORT", 8080))  # Usa 5000 por defecto si no se encuentra la variable
+    app.run(debug=True, host="0.0.0.0", port=port)
